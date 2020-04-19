2b49469cb940135e32d84d50cd5aa5096e7b763d
Fumihisa Suzuki <fumi_s_qrio192@yahoo.co.jp>
2020-04-19 08:18:53 +0000

    Indicate Top page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,48 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+    
+  # 変数セット
+  before_action :set_user
+  
+  $days_of_the_week = %w{日 月 火 水 木 金 土}
+  
+  
+  # beforeフィルター
+
+  # 現在のユーザーを取得します。
+  def set_user
+    if user_signed_in?
+      @user_status = "logged-in-user"
+      @home_page_path = root_path
+      @home_page_name = 'ホーム'
+    else
+      @user_status = "guest-user"
+      @home_page_path = root_path
+      @home_page_name = 'ログイン'
+    end
+    
+=begin
+    if user_signed_in?
+      if params[:user_id].present?
+        @user = User.find(params[:user_id])
+      elsif params[:id].present?
+        @user = User.find(params[:id])
+      end
+      if current_user.admin?
+        @user_status = "admin-user"
+        @home_page_path = users_path
+        @home_page_name = 'ユーザーリスト'
+      else
+        @user_status = "logged-in-user"
+        @home_page_path = user_path(current_user)
+        @home_page_name = 'ホーム'
+      end
+    else
+      @user_status = "guest-user"
+      @home_page_path = root_path
+      @home_page_name = 'ログイン'
+    end
+=end
+  end
+  
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module SpaceAppR1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Asia/Tokyo'
+#    config.i18n.default_locale = :ja # デフォルトの言語を日本語に設定します。
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
