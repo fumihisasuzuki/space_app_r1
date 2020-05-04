c438ae674a9392646104f69773d4440f072be505
Fumihisa Suzuki <fumi_s_qrio192@yahoo.co.jp>
2020-05-04 22:49:27 +0000

    Success Event-create-action normaly

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,55 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %w[show]
+  
   def new
+    @event = Event.new
   end
-  def new_a
-  end
+  
   def new_b
+    @event = Event.new
   end
   
   def create
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
+    if @event.save
+      flash[:success] = 'イベントを作成しました'
+      redirect_to event_path(@event)
+    else
+      #debugger
+      render :new_b
+    end
+  end
+  
+  def index
   end
 
-#  def show
-#  end
+  def show
+  end
+  
+  private
+  
+    # beforeフィルター
+  
+    # 対象のイベントを取得します。
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+
+    # strong parameter
+    def event_params
+      params.require(:event).permit(:event_name,
+                                    :chouseisan_note,
+                                    :chouseisan_url,
+                                    :chouseisan_check,
+                                    :place,
+                                    :indication_price,
+                                    :deadline,
+                                    :reserved_at,
+                                    :reserved_by,
+                                    :reserved_number_of_members,
+                                    :reference)
+    end
+
 end
